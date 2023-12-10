cb0f4eaec2b789ad2d1418abd2f65239f3588373
teodorstupnicki <teodor.stupnicki@gmail.com>
2023-12-10 23:25:30 +0000

    Moved gitignore to other location

--- solutions/4/four/src/main.rs
+++ solutions/4/four/src/main.rs
@@ -5,6 +5,13 @@ fn main() {
     let mut sum = 0;
     for line in contents.lines() {
         let it = line.split("|");
+        let winning = it.next().unwrap().split(" ").collect();
+        winning.next();
+        winning.next();
+        for item in winning {
+            println!("{}", item);
+        }
+        let numbers = it.next().unwrap().split(" ").collect();
         let result = calculate_points(it);
         sum = sum + result;
         break;
@@ -12,6 +19,6 @@ fn main() {
 }
 
 
-fn calculate_points(line: Split<str>) -> i32 {
-   0 
-}
+// fn calculate_points(line: Split<str>) -> i32 {
+   //0 
+// }
